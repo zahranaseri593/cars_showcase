0724fee7eb908780cb062c345a0a6285e6280271
Zahra Naseri <zahranaseri593@gmail.com>
2023-08-14 13:22:46 +0330

    finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-import {CarCard, CustomFilter, Hero, SearchBar} from '@/components'
+import {CarCard, CustomFilter, Hero, SearchBar, ShowMore} from '@/components'
+import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
@@ -27,8 +28,8 @@ export default async function Home({searchParams}) {
           <SearchBar />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels}/>
+            <CustomFilter title="year" options={yearsOfProduction}/>
           </div>
         </div>
 
@@ -37,6 +38,11 @@ export default async function Home({searchParams}) {
               <div className="home__cars-wrapper">
                 {allCars?.map((car) => <CarCard car={car} />)}
               </div>
+
+              <ShowMore
+                pageNumber={(searchParams.pageNumber || 10) / 10}
+                isNext={(searchParams.limit || 10) > allCars.length}
+               /> 
             </section>
           ) :(
             <div className='home__error-container'>
--- types/index.ts
+++ types/index.ts
@@ -31,9 +31,24 @@ export interface CarProps {
 }
 
 export interface FilterProps {
-    manufacturer: string,
-    year: number,
-    fuel: string,
-    limit: number,
-    model: string,
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
+
+export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
 }
